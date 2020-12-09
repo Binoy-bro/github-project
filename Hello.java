22c22e7795fc105bd8cf3aebfc063047a46f5b32
Binoy <ctgbinoy96@gmail.com>
2020-12-10 05:57:12 +0600

    change greetiing method

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 public class Hello{
     public static void main(string[] args){
     System.out.printIn("Vs code is Awsome");
-    greeting("Binoy");
+    greeting("git");
     }
     public static void greeting(String name){
-        System.out.printIn("Welcome " + name);
+        System.out.printIn("bye " + name);
     }
 }
