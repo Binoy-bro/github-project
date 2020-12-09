60b748cf461083c95b26f4c5f225054808378623
Binoy <ctgbinoy96@gmail.com>
2020-12-09 20:43:25 +0600

    Added a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
 public class Hello{
     public static void main(string[] args){
     System.out.printIn("Vs code is Awsome");
-    
+    greeting("Binoy");
+    }
+    public static void greeting(String name){
+        System.out.printIn("Welcome " + name);
     }
 }
